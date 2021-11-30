9ab55220464ebfa86d98a564a95f32dcdb46225b
Fuqiao Xue <xfq@w3.org>
2021-11-30 13:39:44 +0800

    Add simple-ruby-markdown-rs-v1 and v2

---
+++ simple-ruby-markdown-rs-v1/biblio.js
@@ -0,0 +1,8 @@
+respecConfig.localBiblio = {
+    "JISX4051": {
+        "title": "Formatting rules for Japanese documents (&#12302;&#26085;&#26412;&#35486;&#25991;&#26360;&#12398;&#32068;&#29256;&#26041;&#27861;&#12303;; JIS X 4051)",
+        "publisher": "Japanese Standards Association",
+        "date": "2004",
+        "id": "JIS X 4051:2004",
+      }
+};
---
+++ simple-ruby-markdown-rs-v1/respec-config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var respecConfig = {
+    format: "markdown",
+    specStatus: "ED",
+    editors: [
+        { name: "Florian Rivoal", company: "Invited Expert", url: "https://florian.rivoal.net", w3cid: "43241" },
+        { name: "Atsushi Shimono", company: "W3C", w3cid: "110639" },
+        { name: "Richard Ishida", company: "W3C", w3cid: "3439" }
+        ],
+    authors: [
+        { name: "Toshi Kobayashi" }
+        ],
+    github: "w3c/simple-ruby",
+    edDraftURI: "https://w3c.github.io/simple-ruby/",
+    prevED: "https://w3c.github.io/jlreq/docs/simple-ruby/",
+    shortName: "simple-ruby",
+    alternateFormats: [
+        { uri: "ruby-rules-ja.pdf", label: "Original Japanese (PDF)" }
+        ],
+    localBiblio: {
+      JISX4051: {
+        title: "Formatting rules for Japanese documents (&#12302;&#26085;&#26412;&#35486;&#25991;&#26360;&#12398;&#32068;&#29256;&#26041;&#27861;&#12303;; JIS X 4051)",
+        publisher: "Japanese Standards Association",
+        date: "2004",
+        id: "JIS X 4051:2004",
+      },
+    },
+    noRecTrack: true,
+    group: "i18n",
+  };
